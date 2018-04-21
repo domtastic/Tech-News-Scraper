ec3a439a896901cd65ae242f8245170d25299d4b
Dominic <gonza362@gmail.com>
2018-04-21 13:12:52 -0600

    changed stuff

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ require("./routes/api/headline-api-routes")(app);
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/dgscraper";
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URI).catch((err) => {
-   console.log(err);
+mongoose.connect(process.env.MONGODB_URI, {
+    uri_decode_auth:true
 });
 
 
